9645b4bda509b6297fa5dfeb725d35f456850b8e
Abhishekpatidar901 <abhishekpatidar901@gmail.com>
2024-06-24 15:52:49 +0530

    Real Estate App

--- client/src/config.js
+++ client/src/config.js
@@ -5,6 +5,6 @@
 // export const GOOGLE_PLACES_KEY = process.env.GOOGLE_PLACES_KEY;
 // export const GOOGLE_MAPS_KEY = process.env.GOOGLE_MAPS_KEY;
 
-export const API = "http://localhost:8000/api"
+export const API = "https://realestate-app-sfek.onrender.com/api"
 export const GOOGLE_PLACES_KEY ="AIzaSyCYZI5-eCDjutlSiWuQp3U7QVe4E7QwL4k"
 export const GOOGLE_MAPS_KEY ="AIzaSyC0XGm9keZUTqOqyIT8Ku8yBHsPbMc6P_g"
