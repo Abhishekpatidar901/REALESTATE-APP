bbb12525d916dea2fad3158c8efe8ff190d5f6b4
Abhishekpatidar901 <abhishekpatidar901@gmail.com>
2024-06-24 14:54:00 +0530

    Real Estate App

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,10 @@
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 
-export const API = process.env.API;
-export const GOOGLE_PLACES_KEY = process.env.GOOGLE_PLACES_KEY;
-export const GOOGLE_MAPS_KEY = process.env.GOOGLE_MAPS_KEY;
+// export const API = process.env.API;
+// export const GOOGLE_PLACES_KEY = process.env.GOOGLE_PLACES_KEY;
+// export const GOOGLE_MAPS_KEY = process.env.GOOGLE_MAPS_KEY;
+
+export const API = "http://localhost:8000/api"
+export const GOOGLE_PLACES_KEY ="AIzaSyCYZI5-eCDjutlSiWuQp3U7QVe4E7QwL4k"
+export const GOOGLE_MAPS_KEY ="AIzaSyC0XGm9keZUTqOqyIT8Ku8yBHsPbMc6P_g"
